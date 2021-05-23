66d5a06cd81f51b3609ef93c420514015546526d
pgrgrgrgr <84655070+pgrgrgrgr@users.noreply.github.com>
2021-05-23 16:05:57 +0900

    실패율

--- step1/실패율.py
+++ step1/실패율.py
@@ -3,7 +3,6 @@ def solution(N, stages):
     returnlist = []
     stages.sort() 
     failure={}
-    # [1,2,2,2,3,3,4,6]
     for i in range(N):
         try:
             failure[i+1] = stages.count(i+1)/len(stages)
@@ -13,7 +12,4 @@ def solution(N, stages):
     returnlist = sorted(failure.items(),reverse = True,key = lambda x:x[1])
     for i,j in returnlist:
         answer.append(i)
-    print(answer)
     return answer   
-
-solution(5,[1])
