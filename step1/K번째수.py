8197a9a286e66a7defcc061a910098d7092f6ed2
pgrgrgrgr <84655070+pgrgrgrgr@users.noreply.github.com>
2021-05-23 16:00:48 +0900

    K번째수

--- step1/K번째수.py
+++ step1/K번째수.py
@@ -9,9 +9,4 @@ def solution(array, commands):
             new_array.append(array[commands[i][0]-1:commands[i][1]])
             new_array[i].sort()
             answer.append(new_array[i][commands[i][2]-1])
-    print(new_array)
-    print(answer)
-
     return answer
-
-solution([1],[[2,5,1],[4,4,1],[1,7,1]])
