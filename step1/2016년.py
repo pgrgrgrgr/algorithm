44cd19452f766d4ad97543c19db5ddf09a018db6
pgrgrgrgr <84655070+pgrgrgrgr@users.noreply.github.com>
2021-05-25 17:09:29 +0900

    2016년.py
    역시나 파이썬에는 날짜 구하는 함수가 있었다

--- step1/2016년.py
+++ step1/2016년.py
@@ -34,5 +34,5 @@ def solution(a, b):
 # def solution(a,b):
   # days = ['MON','TUE','WED','THU','FRI','SAT','SUN']
   # answer = days[datetime.date(2016,a,b).weekday()]
-  # return answer
+  # return answer 
   
