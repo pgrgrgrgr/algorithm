8b8a978485747490c40c5aeee7e6a3e0bb7479bd
pgrgrgrgr <parkracoon@naver.com>
2021-05-27 09:50:30 +0900

    같은숫자는싫어

--- step1/같은숫자는싫어.py
+++ step1/같은숫자는싫어.py
@@ -1,7 +1,6 @@
 import re
 
 def solution(arr):
-
   answer = []
   binstr=''
   for i in range(len(arr)):
@@ -9,6 +8,4 @@ def solution(arr):
   for i in range(10):
     binstr = re.sub('%d+'%i,'%d '%i,binstr)
   answer = list(map(int,binstr.split()))
-  return answer
-
-solution([1,1,3,3,0,1,1])
+  return answer 
