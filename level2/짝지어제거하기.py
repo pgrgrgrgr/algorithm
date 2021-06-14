6e559426850fe66d3e0aafc30e502904e12c6ced
pgrgrgrgr <parkracoon@naver.com>
2021-06-14 18:06:56 +0900

    programmers.level2

---
+++ level2/짝지어제거하기.py
@@ -0,0 +1,26 @@
+import re
+
+def solution(s):
+  sstack=[]
+  s=list(s)
+  while s:
+    sstack.append(s.pop())
+    if len(sstack) > 1 and sstack[-1] == sstack[-2]:
+      sstack.pop()
+      sstack.pop()
+  if(len(sstack)==0):
+    return 1
+  else:
+    return 0
+
+# def solution(s):
+#   answer = []
+#   for i in s:
+#       if not(answer):
+#           answer.append(i)
+#       else:
+#           if(answer[-1] == i):
+#               answer.pop()
+#           else:
+#               answer.append(i)    
+#   return not(answer)
