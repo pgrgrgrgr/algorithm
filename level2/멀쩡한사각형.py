0b75c99fd60472d39b77651dd2b1cb51fcc672f6
pgrgrgrgr <parkracoon@naver.com>
2021-06-16 19:59:15 +0900

    멀쩡한사각형

--- level2/멀쩡한사각형.py
+++ level2/멀쩡한사각형.py
@@ -1,9 +1,14 @@
+# import math
+
+# def solution(w,h):
+#   answer = 0
+#   cnt=1
+#   while cnt != w:
+#     answer += math.floor(-h/w * cnt + h)
+#     cnt += 1
+#   return answer*2
+
 import math
 
 def solution(w,h):
-  answer = 0
-  cnt=1
-  while cnt != w:
-    answer += math.floor(-h/w * cnt + h)
-    cnt += 1
-  return answer*2
+  return w*h - (w+h-math.gcd(w,h))
