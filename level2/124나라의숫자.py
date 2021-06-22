ade411ebfab2e4dc94945c4abff89a28df7b3da7
pgrgrgrgr <parkracoon@naver.com>
2021-06-22 18:22:51 +0900

    124나라의 숫자

--- level2/124나라의숫자.py
+++ level2/124나라의숫자.py
@@ -10,3 +10,16 @@ def solution(n):
       n = int(n/3)
       
   return answer
+
+# def change124(n):
+#   num = ['1','2','4']
+#   answer = ""
+
+
+#   while n > 0:
+#     n -= 1
+#     answer = num[n % 3] + answer
+#     n //= 3
+
+#   return answer
+# if문이 필요가 없음
