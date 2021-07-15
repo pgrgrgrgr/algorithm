ee648a609cb9ba7d77e930c714941082ffbf8d22
pgrgrgrgr <parkracoon@naver.com>
2021-07-15 19:32:48 +0900

    stack,queue

---
+++ level2/다리를지나는트럭.py
@@ -0,0 +1,18 @@
+def solution(bridge_length, weight, truck_weights):
+  answer = 0
+  second = 0
+  bridge = [0] * bridge_length
+    
+  while bridge:
+    second += 1
+    bridge.pop(0)
+    if truck_weights:
+      if sum(bridge) + truck_weights[0] <= weight:
+        bridge.append(truck_weights.pop(0))
+      else:
+        bridge.append(0)
+  
+  answer = second
+  return answer
+
+solution(100,100,[10])
