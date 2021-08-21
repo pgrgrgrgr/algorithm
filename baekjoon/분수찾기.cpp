ba55c53bed802ef1fa28ea44af0ebf93ea5269a7
pgrgrgrgr <84655070+pgrgrgrgr@users.noreply.github.com>
2021-08-21 16:30:26 +0900

    math, if

--- baekjoon/분수찾기.cpp
+++ baekjoon/분수찾기.cpp
@@ -2,17 +2,21 @@
 
 int main() {
 	int a;
-	int chk = 1;
+	int chk = 0;
 	int tmp = 1;
-	
+
 	std::cin >> a;
 
 	while (a > chk) {
 		chk += tmp;
 		tmp++;
 	}
-	std::cout << chk - a / tmp - (chk - a);
-
-	return chk - a / tmp - (chk - a);
-
+	if (tmp % 2 == 0) {
+		std::cout << (chk - a + 1) << "/" << tmp - (chk - a + 1);
+	}
+	else {
+		std::cout << tmp - (chk - a + 1) << "/" << chk - a + 1;
+	}
+	
+	return 0;
 }
