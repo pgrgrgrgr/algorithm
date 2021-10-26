ac4689c8670c762427aa8de31b285adf944eb1ba
pgrgrgrgr <84655070+pgrgrgrgr@users.noreply.github.com>
2021-10-26 18:28:33 +0900

    queue, 2164

---
+++ baekjoon/카드2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	queue<int> cards;
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cards.push(i);
+	}
+	while (!(cards.size() == 1)) {
+		cards.pop();
+		cards.push(cards.front());
+		cards.pop();
+	}
+	cout << cards.front();
+
+	return 0;
+}
