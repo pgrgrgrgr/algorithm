77affad42294bbfe06a4ce212eca774b308ea8a6
pgrgrgrgr <parkracoon@naver.com>
2023-01-05 19:16:17 +0900

    push & pop, but wrong solving

--- programmers/level1/과일장수.py
+++ programmers/level1/과일장수.py
@@ -10,4 +10,7 @@ def solution(k, m, score):
     sum_score.clear()
   return answer
 
+# def solution(k, m, score):
+  # return sum(sorted(score)[len(score)%m::m])*m
+
 print(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
